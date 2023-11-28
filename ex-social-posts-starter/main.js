1b4f40711a866362efd497578cb53f0b6f6279ca
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-28 16:02:35 +0100

    inseriti i post

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -58,26 +58,27 @@ const posts = [
 
 
 
-const containerElement = document.querySelector('posts-list');
+const containerElement = document.querySelector('div.posts-list');
 
 for (let i = 0; i < posts.length; i++){
     let post = posts[i]
-    
+    const postCards = document.createElement('article');
+    postCards.innerHTML =
     `<div class="post">
         <div class="post__header">
             <div class="post-meta">                    
                 <div class="post-meta__icon">
-                    <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                    <img class="profile-pic" src=${post.image} alt="Phil Mangione">                    
                 </div>
                 <div class="post-meta__data">
-                    <div class="post-meta__author">Phil Mangione</div>
-                    <div class="post-meta__time">4 mesi fa</div>
+                    <div class="post-meta__author">${post.name}</div>
+                    <div class="post-meta__time">${post.created}</div>
                 </div>                    
             </div>
         </div>
-        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+        <div class="post__text">${post.content}</div>
         <div class="post__image">
-            <img src="https://unsplash.it/600/300?image=171" alt="">
+            <img src=${post.media} alt="">
         </div>
         <div class="post__footer">
             <div class="likes js-likes">
@@ -88,9 +89,11 @@ for (let i = 0; i < posts.length; i++){
                     </a>
                 </div>
                 <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
                 </div>
             </div> 
         </div>            
     </div>`
+
+    containerElement.appendChild(postCards)
 }
